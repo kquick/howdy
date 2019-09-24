6cb626531f9c5f35bec840d57031dc0a1f605f87
Kevin Quick <kquick@galois.com>
2019-09-23 23:42:38 -0700

    Added repeating greetings.

--- howdy.cpp
+++ howdy.cpp
@@ -3,8 +3,11 @@
 
 extern const std::string whom();
 
+const char* greetings[5] = { "Howdy, ", "Hello, ", "Hi, ", "Hey! ", "Yo " };
+
 int main()
 {
-    std::cout << "Howdy, " << whom() << "!" << std::endl;
+    for (int ii = 0; ii < 5; ++ii)
+        std::cout << greetings[ii] << whom() << "!" << std::endl;
     return 0;
 }
